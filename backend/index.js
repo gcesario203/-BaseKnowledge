a0939759e3ac3d34c3d6082352565fc6e7dba771
Gabriel Cesario <cesario203@outlook.com>
2020-07-11 16:09:26 -0300

    Preparando configurações de middlewares com cors e bodyParser e instanciando consign para auxilio de nossas importações

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,10 @@
 const app = require('express')()
 const port = 3000
+const consign = require('consign')
+
+consign()
+    .then('./config/middlewares.js')
+    .into(app)
 
 app.listen(port,()=>{
     console.log('Backend iniciado com sucesso')
