81043a736d583a0c78cc32d3fe1f8f2311ae31a2
Gabriel Cesario <cesario203@outlook.com>
2020-07-14 00:52:35 -0300

    Fazendo requisições mostrar os usuário cadastrados

--- frontend/src/components/admin/UserAdmin.vue
+++ frontend/src/components/admin/UserAdmin.vue
@@ -1,12 +1,40 @@
 <template>
    <div class="user-admin">
-       <h1>COmponente Usuário</h1>
+       <b-table hover striped :items="users" :fields="fields">
+       </b-table>
    </div>
 </template>
 
 <script>
+import axios from 'axios'
+import {baseApiUrl} from '../../global'
+
 export default {
-    name:'UserAdmin'
+    name:'UserAdmin',
+    data: () =>{
+        return{
+            mode:'save',
+            user:{},
+            users:[],
+            fields:[
+                {key:'id', label:'Código', sortable: true},
+                {key:'name',label:'Nome', sortable:true},
+                {key:'email',label:'E-Mail', sortable:true},
+                {key:'admin',label:'Administrador', sortable:true, formatter: value=> value?'Sim':'Não'},
+                {key:'actions',label:'Ações'}
+            ]
+        }
+    },
+    methods:{
+        loadUsers(){
+            axios.get(`${baseApiUrl}/users`).then(res=>{
+                this.users = res.data
+            })
+        }
+    },
+    mounted(){
+        this.loadUsers()
+    }
 }
 </script>
 
